786364a384367bfedd60803f406272649ba3c144
Azat Akmyradov <azat@rebdev.com>
2024-03-18 14:10:40 -0400

    fix: remove mocks

--- Tests/Feature/Controllers/ProductionReportingController/IndexTest.php
+++ Tests/Feature/Controllers/ProductionReportingController/IndexTest.php
@@ -3,8 +3,6 @@
 namespace Modules\ProductionReporting\Tests\Feature\Controllers\ProductionReportingController;
 
 use Illuminate\Support\Facades\Auth;
-use Mockery;
-use Modules\ProductionReporting\WebServices\WorkOrderWebService;
 use function Pest\Laravel\get;
 
 uses(\Tests\TestCase::class);
@@ -12,33 +10,14 @@ uses(\Tests\TestCase::class);
 beforeEach(function () {
     $this->withoutExceptionHandling();
     Auth::loginUsingId(1);
-
-    Mockery::mock('overload:' . WorkOrderWebService::class)
-        ->makePartial()
-        ->expects()
-        ->all('NA023')
-        ->andReturn(collect([
-            [
-                'MFGNUM' => '123',
-            ],
-        ]));
 });
 
 it('should return correct component', function () {
     get('/production-reporting')
-        ->hasComponent('ProductionReporting/resources/views/Index')
-        ->hasResource('work_orders', [
-            [
-                'MFGNUM' => '123',
-            ]
-        ]);
+        ->hasModuleComponent('ProductionReporting', 'Index');
 });
 
 it('should pass correct props', function () {
     get('/production-reporting')
-        ->hasResource('work_orders', [
-            [
-                'MFGNUM' => '123',
-            ]
-        ]);
+        ->hasResource('work_orders');
 });
