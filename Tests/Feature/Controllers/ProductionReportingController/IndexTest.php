6677aa5e4e2dd7715f24f07e541bb588f6fa9fd6
Azat Akmyradov <azat@rebdev.com>
2024-03-19 22:33:52 -0400

    fix: feature tests with fake requests

--- Tests/Feature/Controllers/ProductionReportingController/IndexTest.php
+++ Tests/Feature/Controllers/ProductionReportingController/IndexTest.php
@@ -4,23 +4,37 @@ declare(strict_types=1);
 
 namespace Modules\ProductionReporting\Tests\Feature\Controllers\ProductionReportingController;
 
-use Illuminate\Support\Facades\Auth;
-
+use function Pest\Laravel\actingAs;
 use function Pest\Laravel\get;
 
 uses(\Tests\TestCase::class);
 
-beforeEach(function () {
-    $this->withoutExceptionHandling();
-    Auth::loginUsingId(1);
-});
-
 it('should return correct component', function () {
+    actingAs(createAdmin());
+
+    fakeHttp([
+        'PARAM_OUT' => [],
+        'PARAM_STAT' => [
+            'O_STAT' => 1,
+            'O_MESSAGE' => '',
+        ]
+    ]);
+
     get('/production-reporting')
         ->hasModuleComponent('ProductionReporting', 'Index');
 });
 
 it('should pass correct props', function () {
+    actingAs(createAdmin());
+
+    fakeHttp([
+        'PARAM_OUT' => [],
+        'PARAM_STAT' => [
+            'O_STAT' => 1,
+            'O_MESSAGE' => '',
+        ]
+    ]);
+
     get('/production-reporting')
         ->hasResource('work_orders');
 });
